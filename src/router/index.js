cffe5e5d9bfec113605dd98edb860013c6130cda
zhouqyi5 <zhouqyi5@163.com>
2022-06-28 23:19:30 +0800

    init project

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   component: () => import('../views/About.vue')
+  // }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
