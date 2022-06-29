05bf8e41e57d1eaff99e6ddb3dfa7575c63688d8
zhouqyi5 <zhouqyi5@163.com>
2022-06-29 13:47:02 +0800

    Button按钮

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import 'normalize.css' // 三方重置样式包
+import '@/assets/styles/common.less' // 重置样式
+
+import Fine from '@/components' // 自己的Fine-UI组件库并且注册use(Fine)
+
+createApp(App).use(store).use(router).use(Fine).mount('#app')
