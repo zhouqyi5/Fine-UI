745f60fddfc93ae339abfec7aca73d526c50bc03
zhouqyi5 <zhouqyi5@163.com>
2022-07-04 22:16:16 +0800

    bread面包屑

---
+++ src/components/FnBread.vue
@@ -0,0 +1,40 @@
+<!-- 面包屑组件 -->
+<script>
+import { h } from 'vue'
+export default {
+  name: 'FnBread',
+  render() {
+    const items = this.$slots.default()
+    const dymanicItems = []
+    items.forEach((item, i) => {
+      dymanicItems.push(item)
+      if (i < items.length - 1) {
+        dymanicItems.push(h('i', { class: 'icon fine-arrow-double-right' }))
+      }
+    })
+    return h('div', { class: 'fn-bread' }, dymanicItems)
+  }
+}
+</script>
+
+<style lang="less">
+.fn-bread {
+  display: flex;
+  padding: 25px 10px;
+  &-item {
+    a {
+      color: #666;
+      transition: all 0.4s;
+      &:hover {
+        color: #05c364;
+      }
+    }
+  }
+  i {
+    font-size: 12px;
+    margin-left: 5px;
+    margin-right: 5px;
+    line-height: 22px;
+  }
+}
+</style>
---
+++ src/components/FnBreadItem.vue
@@ -0,0 +1,18 @@
+<!-- 面包屑组件-高级级(两个组件配合使用) -->
+<template>
+  <div class="fn-bread-item">
+    <RouterLink :to="to" v-if="to"><slot /></RouterLink>
+    <span v-else><slot /></span>
+  </div>
+</template>
+<script>
+export default {
+  name: 'FnBreadItem',
+  props: {
+    to: {
+      type: [String, Object],
+      default: ''
+    }
+  }
+}
+</script>
